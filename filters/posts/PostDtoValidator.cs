d1a4496bc4039d0e34319e42d7c10f306e728314
Alekssandher <alekssandher1@hotmail.com>
2025-02-16 12:34:04 -0300

    feat: PostDto media FluentValidation

--- filters/posts/PostDtoValidator.cs
+++ filters/posts/PostDtoValidator.cs
@@ -10,17 +10,21 @@ namespace reichan_api.Filters {
             ApplyStringValidationRules(x => x.Title);
             ApplyStringValidationRules(x => x.Content);
             ApplyStringValidationRules(x => x.Author!);
-            ApplyStringValidationRules(x => x.Media);
-
+            
             ApplyPublicKeyAndSignatureRules(x => x.AuthorPubKey!);
             ApplyPublicKeyAndSignatureRules(x => x.Signature!);
+
+            RuleFor(x => x.Media)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .Matches(@"^[a-zA-Z0-9.-]+$").WithMessage("{PropertyName} contains invalid characters.")
+                .When(x => !string.IsNullOrEmpty(x.Media));
         }
 
         private void ApplyStringValidationRules(Expression<Func<PostDto, string>> property)
         {
             RuleFor(property)
                 .NotEmpty().WithMessage("{PropertyName} is required.")
-                .Matches(@"^[a-zA-Z0-9\s.,!?-]+$").WithMessage("{PropertyName} contains invalid characters.")
+                .Matches(@"^[a-zA-Z0-9\s.,!?'-]+$").WithMessage("{PropertyName} contains invalid characters.")
                 .When(property => !string.IsNullOrEmpty(property.Author));
         }
         private void ApplyPublicKeyAndSignatureRules(Expression<Func<PostDto, string>> property)
